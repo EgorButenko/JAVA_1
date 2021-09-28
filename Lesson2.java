73732c99a9052761af5d81dd0817d32f972a7bef
EgorButenko <90284428+EgorButenko@users.noreply.github.com>
2021-09-28 18:05:43 +0300

    Update and rename Lesson_2 to Lesson2.java

--- Lesson2.java
+++ Lesson2.java
@@ -3,7 +3,7 @@
 	@author Egor Butenko
 	@version 20.09.21
 */
-public class HomeWorkApp {
+public class Lesson2 {
 		public static void main(String[] args){
 			
 			System.out.println(summNumber(5, 4));
